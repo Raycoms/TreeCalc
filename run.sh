84ce340b27ee4699ed805a8bbc5ffe6fc320e4cf
Raycoms <ray.neiheiser@gmail.com>
2025-08-21 20:08:49 +0800

    fix run file

--- run.sh
+++ run.sh
@@ -3,14 +3,13 @@
 #tc qdisc add dev lo root netem delay 200ms
 ulimit -n 100000
 
-configurations=(256 320)
-cores=(2 4 6 8 10 12 14 16)
-participationrate=(6 20 200)
+configurations="256 320"
+cores="2 4 6 8 10 12 14 16"
+participationrate="6 20 200"
 
-# Iterate using indices
-for x in "${configurations[@]}"; do
-    for y in "${cores[@]}"; do
-        for z in "${participationrate[@]}"; do
+for x in $configurations; do
+    for y in $cores; do
+        for z in $participationrate; do
             echo "Running Configuration: $x Cores: $y Participation Rate $z"
             exec /usr/local/bin/tree $x $y $z | grep "final"
         done
